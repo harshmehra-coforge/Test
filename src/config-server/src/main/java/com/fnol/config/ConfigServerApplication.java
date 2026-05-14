9352d86a385c3c941718b49e360623d426a444ff
harshmehra-coforge <harsh.mehra@coforge.com>
2026-05-14 17:41:00 +0530

    Add generated FNOL microservices code from Deep Agent Forgex: config-server/src/main/java/com/fnol/config/ConfigServerApplication.java

---
+++ src/config-server/src/main/java/com/fnol/config/ConfigServerApplication.java
@@ -0,0 +1,22 @@
+package com.fnol.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Spring Cloud Config Server
+ * 
+ * Provides centralized configuration management for all FNOL microservices.
+ * Configurations are stored in a Git repository and served to client applications.
+ */
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
