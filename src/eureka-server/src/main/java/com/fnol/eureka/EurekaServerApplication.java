49d4a408556f958881a9bad3fc6650afd2738c32
harshmehra-coforge <harsh.mehra@coforge.com>
2026-05-14 17:41:13 +0530

    Add generated FNOL microservices code from Deep Agent Forgex: eureka-server/src/main/java/com/fnol/eureka/EurekaServerApplication.java

---
+++ src/eureka-server/src/main/java/com/fnol/eureka/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.fnol.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka Service Discovery Server
+ * 
+ * This server provides service registration and discovery capabilities
+ * for the FNOL microservices architecture.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
